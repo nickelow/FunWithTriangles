4d07c478da108ef3eb67c2570f2e37f67f903a4c
Matthew Nicolaou <nickelow@users.noreply.github.com>
2018-02-17 17:02:08 -0800

    Update triangles.js

--- triangles.js
+++ triangles.js
@@ -23,10 +23,12 @@ module.exports = {
   	const sideA = triangle[0];
   	const sideB = triangle[1];
   	const sideC = triangle[2];
+	 //Check to make sure all of the sides are greater than 0
 		if(triangle.filter(side=>side <= 0).length > 0){
 			console.log('Triangle sides must be greater than 0');
 			return false;
 		}
+	  //Check to make sure all of the sides adhere to the geometric conditions of a triangle. 
 		if((sideA + sideB) <= sideC || (sideA + sideC) <= sideB || (sideB+ sideC) <= sideA){
 			console.log('The sum of 2 sides must always be greater than the other side');
 			return false; 
@@ -42,7 +44,10 @@ module.exports = {
       	2: 'isoceles',
       	3: 'scalene'
       }
+      //Create a set using the values given
       const numberOfDifferentSides = new Set(triangle)
+     //The size of the set tells you how many unique values were in the array, and each number 1-3  corresponds
+     // to a property in the triangleTypes object whose value is a string representing the correct triangle type
       return triangleTypes[numberOfDifferentSides.size]
 		} else {
 			return 'invalid triangle';
